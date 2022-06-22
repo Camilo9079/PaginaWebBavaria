b6dc16d1f26f0f97fc39fb0dab45f1a780f98ebe
Camilo9079 <106204611+Camilo9079@users.noreply.github.com>
2022-06-22 17:42:45 -0500

    Initial Commit
    Web page based in bavaria.com

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>{ 
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.noticias', {delay:500});
+ScrollReveal().reveal('.banner-1', {delay:500});
+ScrollReveal().reveal('.banner-2', {delay:500});
